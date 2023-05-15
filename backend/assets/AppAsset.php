6440e8bfcc5e721ff3c60658d4c20e0d3c313b68
Sparky <mmd@9-33.com>
2023-05-15 11:56:09 +0300

    Подключила font-awsome для кастомизации кнопок виджетов.

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,11 +13,12 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
     ];
     public $js = [
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap5\BootstrapAsset',
+        'yii\bootstrap5\BootstrapAsset'
     ];
 }
